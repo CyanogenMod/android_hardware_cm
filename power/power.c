29efe6b33b4421ce1a28f3e4a612c805219faa82
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-05-05 14:33:26 +0200

    correct path of cpu.notify_on_migrate
    Change-Id: I3fa7cc4c477113677f53bc4034cd77bb0eb24117

--- power/power.c
+++ power/power.c
@@ -29,7 +29,7 @@
 #define SCALING_GOVERNOR_PATH "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
 #define BOOSTPULSE_ONDEMAND "/sys/devices/system/cpu/cpufreq/ondemand/boostpulse"
 #define BOOSTPULSE_INTERACTIVE "/sys/devices/system/cpu/cpufreq/interactive/boostpulse"
-#define NOTIFY_ON_MIGRATE "/dev/cpuctl/apps/cpu.notify_on_migrate"
+#define NOTIFY_ON_MIGRATE "/dev/cpuctl/cpu.notify_on_migrate"
 
 struct cm_power_module {
     struct power_module base;
